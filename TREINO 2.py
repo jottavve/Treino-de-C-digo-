311fdee3b59a04edc764d391845d7ce155feac44
joão vitu <jojoguel05@gmail.com>
2026-03-12 13:47:32 -0300

    Add files via upload

---
+++ TREINO 2.py
@@ -0,0 +1,10 @@
+idade = float(input("Digite a sua idade: "))
+
+if idade < 12:
+    print("Criança")
+
+elif idade < 18:
+    print("Adolescente")
+
+else:
+    print("Adulto")
---
+++ TREINO.py
@@ -0,0 +1,13 @@
+clima = float(input("Coloque a temperatura: "))
+
+if clima < 15:
+    print("Está frio")
+
+elif 15 <= clima <= 20:
+    print("Ameno")
+
+else:
+    print("Está quente ")
+
+
+#Fiz de primeira, fiquei com dúvida no else e pedi ajuda a ele em apenas isso ^_^
